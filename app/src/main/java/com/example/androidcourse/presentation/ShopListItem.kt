4e7de992520194b1a52a377fce066f82598bf421
Tatyana Shkolnik <t.shkolnik@tinkoff.ru>
2024-05-05 14:50:18 +0300

    add ShopListItem

--- app/src/main/java/com/example/androidcourse/presentation/ShopListItem.kt
+++ app/src/main/java/com/example/androidcourse/presentation/ShopListItem.kt
@@ -7,8 +7,9 @@ data class ShopListItem(
     val name: String,
     val count: Int,
     val enabled: Boolean,
-    var id: Int = UNDEFINED_ID
+    val id: Int = UNDEFINED_ID
 ) {
+
     companion object {
         const val UNDEFINED_ID = 0
     }
